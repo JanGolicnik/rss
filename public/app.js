429e07347d98e966387b5e31ef5cb874e74682eb
Jan Goličnik <jan@nejka.net>
2026-06-28 13:16:46 +0200

    asd

--- public/app.js
+++ public/app.js
@@ -11,7 +11,7 @@ let iframe = document.getElementById("site-preview");
 document.addEventListener("mousemove", function (e) {
   let link = e.target.closest("a[data-go]");
   if (!link) return;
-  if (iframe.src !== link.href) {
+  if (getComputedStyle(iframe).display !== "none" && iframe.src !== link.href) {
     iframe.src = link.href;
   }
 });
