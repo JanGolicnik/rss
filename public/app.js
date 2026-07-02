a052841c58d1d2630732c05d50ceb26b164074e5
Jan Goličnik <jan@nejka.net>
2026-07-02 21:43:26 +0200

    asd

--- public/app.js
+++ public/app.js
@@ -1,12 +1,21 @@
 const iframe = document.getElementById("site-preview");
 let selected = null;
 
-document.getElementById("btn_subscribe").addEventListener("click", async () => {
+const btn_subscribe = document.getElementById("btn_subscribe");
+let has_registration = false;
+navigator.serviceWorker.getRegistrations().then((regs) => {
+  has_registration = regs.length > 0;
+  console.log(has_registration);
+  console.log(regs);
+  if (has_registration) btn_subscribe.innerText = "unsub";
+});
+
+btn_subscribe.addEventListener("click", async () => {
   try {
-    await try_subscribe();
+    await (has_registration ? unsubscribe() : try_subscribe());
+    setTimeout(() => location.reload(), 500);
   } catch (e) {
     console.error(e);
-    alert(e);
   }
 });
 
@@ -250,21 +259,40 @@ function urlBase64ToUint8Array(base64String) {
 
 const PUBLIC_KEY =
   "BIDcPJ2Qc6K0_DqTtXwTjwp5AisVwpIAM2RsesvCbSJmv2eXAkX2b0RmS2an7Z3ot-Wvx1nytihMC2PERp40gRk";
+
 async function try_subscribe() {
   const permission = await Notification.requestPermission();
   if (permission !== "granted") return;
 
-  const sw = await navigator.serviceWorker.register("/public/sw.js");
-  const subscription = await sw.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(PUBLIC_KEY),
-  });
+  const reg = await navigator.serviceWorker.register("/public/sw.js");
+  if (!reg.active) {
+    await new Promise((resolve) => {
+      const worker = reg.installing || reg.waiting;
+      if (!worker) return resolve();
+      worker.addEventListener("statechange", () => {
+        if (worker.state === "activated") resolve();
+      });
+    });
+  }
 
   await fetch("/subscribe", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(subscription),
+    body: JSON.stringify(
+      await reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(PUBLIC_KEY),
+      }),
+    ),
   });
+
+  location.reload();
+}
+
+async function unsubscribe() {
+  const regs = await navigator.serviceWorker.getRegistrations();
+  await Promise.all(regs.map((reg) => reg.unregister()));
+  location.reload();
 }
 
 function init() {
